681d225e97d4c644c9f405904046f43fde2eac83
fmway <fm18lv@gmail.com>
2024-02-14 10:46:13 +0700

    add params random

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,22 @@
 import { getRandomEmoji } from "$functions/emoji.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+interface Data {
+  random?: boolean,
+}
 
-export default function Home() {
+export const handler: Handlers<Data> = {
+  GET(_req, ctx) {
+    const params = ctx.url.searchParams;
+    return ctx.render({
+      random: params.has('random'),
+    });
+  },
+};
+
+export default function Home(props: PageProps<Data>) {
   return (
   <div class="w-screen h-screen flex items-center justify-center text-8xl">
-    <p>{ getRandomEmoji() }</p>
+    <p>{ props.data.random ? getRandomEmoji() : '😘' }</p>
   </div>
   );
 }
