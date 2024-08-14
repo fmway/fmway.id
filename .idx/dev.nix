12bbf32c6253354a44a76642ce7c8cdf1c3315f4
fmway <fm18lv@gmail.com>
2024-08-14 08:34:25 +0700

    fmway.nix...?😳

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,12 +1,18 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: let
+  fmway-nix = builtins.fetchGit {
+    url = "https://github.com/fmway/fmway.nix";
+    ref = "refs/tags/v1.0.0";
+  };
+
+  inherit (import fmway-nix { inherit lib; }) initIDXFromDevbox;
+in initIDXFromDevbox ../devbox.json pkgs (self: devbox: {
   # Which nixpkgs channel to use.
   channel = "unstable"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = with pkgs; [
-    deno
   ];
 
   # Sets environment variables in the workspace
@@ -22,11 +28,11 @@
     # Enable previews
     previews = {
       enable = true;
-      previews = {
+      previews = let
+        inherit (devbox.shell) scripts;
+      in {
         web = {
-          # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-          # and show it in IDX's web preview panel
-          command = ["deno" "task" "start"];
+          command = lib.splitString " " scripts.dev;
           manager = "web";
           env = {
             # Environment variables to set for your server
@@ -50,4 +56,4 @@
       };
     };
   };
-}
+})
