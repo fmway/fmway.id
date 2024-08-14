ab598327d3eb9429e73a4029a4856309ab17c13a
fmway <fm18lv@gmail.com>
2024-08-14 18:02:54 +0700

    ...

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
-/// <reference lib="deno.unstable" />"
+/// <reference lib="deno.unstable" />
 
 import "$std/dotenv/load.ts";
 
@@ -11,5 +11,5 @@ import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-Deno.cron("simple cron", "*/5 * * * *", () => console.log("hello"))
+Deno.cron("simple cron", "* * * * *", () => console.log("hello"))
 await start(manifest, config);
