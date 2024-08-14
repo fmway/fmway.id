918230ec3145dc8799983efa10d9a5c53e918252
fm18lv <fm18lv@gmail.com>
2024-08-14 02:29:22 +0000

    update deno

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,5 +1,3 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, lib, ... }: let
   fmway-nix = builtins.fetchGit {
     url = "https://github.com/fmway/fmway.nix";
@@ -9,14 +7,18 @@
   inherit (import fmway-nix { inherit lib; }) initIDXFromDevbox;
 in initIDXFromDevbox ../devbox.json pkgs (self: devbox: {
   # Which nixpkgs channel to use.
-  channel = "unstable"; # or "unstable"
+  channel = "unstable";
 
   # Use https://search.nixos.org/packages to find packages
   packages = with pkgs; [
+    fish
+    starship
   ];
 
   # Sets environment variables in the workspace
-  env = {};
+  env = {
+    SHELL = "${lib.getExe pkgs.fish}";
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -46,13 +48,10 @@ in initIDXFromDevbox ../devbox.json pkgs (self: devbox: {
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        starship-fish = "starship init fish >> ~/.config/fish/config.fish";
       };
       # Runs when the workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
       };
     };
   };
