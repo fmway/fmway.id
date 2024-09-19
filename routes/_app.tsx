1d93ac571f24351e48e27f205a5c442a985b368b
Mohamad Fikri <fm18lv@gmail.com>
2024-09-19 07:32:41 +0700

    Update domain 😍

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,19 +7,19 @@ export default function App({ Component }: PageProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Way to Future Moments (FmWay)" />
         {/* Facebook Meta Tags */}
-        <meta property="og:url" content="https://fmway.me" />
+        <meta property="og:url" content="https://fmway.id" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="FmWay" />
         <meta property="og:description" content="Way to Future Moments (FmWay)" />
-        <meta property="og:image" content="/images/artoo-small.png" />
+        <meta property="og:image" content="/images/fmway.png" />
   
         {/* Twitter Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta property="twitter:domain" content="fmway.me" />
-        <meta property="twitter:url" content="https://fmway.me" />
+        <meta property="twitter:url" content="https://fmway.id" />
         <meta name="twitter:title" content="FmWay" />
         <meta name="twitter:description" content="Way to Future Moments (FmWay)" />
-        <meta name="twitter:image" content="/images/artoo-small.png" />
+        <meta name="twitter:image" content="/images/fmway.png" />
         <title>FmWay</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
