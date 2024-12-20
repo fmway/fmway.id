5b33861f60dbb01615654471b7ca023f84ffa086
fmway <fm18lv@gmail.com>
2024-12-20 22:11:47 +0700

    feat(*): add url shortener

--- routes/_middleware.tsx
+++ routes/_middleware.tsx
@@ -1,6 +1,7 @@
 import { FreshContext } from "$fresh/server.ts";
 import { getRedirectUrl } from "$functions/social-media.ts";
 import { getRandomEmoji } from "$functions/emoji.ts";
+import { nestingRequest } from "@fmway/url-shortener";
 
 export async function handler(req: Request, ctx: FreshContext): Promise<Response> {
   const url = new URL(req.url);
@@ -8,6 +9,10 @@ export async function handler(req: Request, ctx: FreshContext): Promise<Response
   const sosmed = getRedirectUrl(path);
   if (sosmed) {
     return Response.redirect(sosmed);
+  }
+
+  if (path.slice(0, 3) == '/s/') {
+    return nestingRequest("/s/").fetch(req);
   }
 
   if (path === '/' && url.searchParams.has('image')) {
