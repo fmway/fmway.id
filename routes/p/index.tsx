ad30e50fade05b6957e761c4335efd6d1ed34a94
fmway <fm18lv@gmail.com>
2025-01-13 12:09:53 +0700

    todo: add portfolio

---
+++ routes/p/index.tsx
@@ -0,0 +1,7 @@
+export default function Portfolio() {
+  return (
+    <div class="flex justify-center items-center min-h-screen">
+      <span class="text-3xl font-bold">Coming Soon</span>
+    </div>
+  )
+}
